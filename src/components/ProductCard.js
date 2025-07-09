271077084e5f75fadaf672c4103fb3b1fb19db68
Anvitha-goda <anvitha.tammana@godainfotech.in>
2025-07-09 12:09:14 +0530

    implemented global states's context and reducer

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,15 +1,34 @@
+import { useEffect,useState } from "react";
+import { useCart } from "../context/CartContext";
 import "./ProductCard.css";
 
 export const ProductCard = ({product}) => {
-  const {name, price, image} = product;
+  const { cartList,addToCart,removeFromCart } = useCart();
+  const {id,name, price, image} = product;
+  const [isInCart,setIsInCart] = useState(false);
 
-  return (
+  useEffect(() => {
+    const productIsInCart = cartList.find(cartItem => cartItem.id === id); 
+    if(productIsInCart){
+      setIsInCart(true);
+    }else{
+      setIsInCart(false);
+    }
+
+  },[cartList,id])
+
+  function handleAdd(){
+    addToCart(product);
+    console.log(cartList);
+  }
+
+  return (   
     <div className="productCard">
       <img src={image} alt={name} />
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button>Add To Cart</button>
+        { isInCart ? <button className="remove" onClick={() => removeFromCart(product) }>Remove</button> : (<button onClick={handleAdd}>Add To Cart</button>)}
       </div>
     </div>
   )
---
+++ src/context/CartContext.js
@@ -0,0 +1,67 @@
+import { createContext, useReducer } from "react";
+import { useContext } from "react";
+import { cartReducer } from "../reducer/cartReducer";
+
+const initialState = {
+    cartList: [],
+    total: 0
+}
+
+export const CartContext = createContext(initialState);
+
+export const CartProvider = ({children}) => {
+
+    const [ state,dispatch ] = useReducer(cartReducer,initialState);
+
+    const addToCart = (product) => {
+        const updatedCartList = state.cartList.concat(product);
+        updateTotal(updatedCartList);
+
+        dispatch({
+            type: "ADD_TO_CART",
+            payload: {
+                products: updatedCartList
+            }
+        })
+    }
+
+    const removeFromCart = (product) => {
+        const deletedCartList = state.cartList.filter(current => current.id !== product.id);
+        updateTotal(deletedCartList);
+
+        dispatch({
+            type: "REMOVE_FROM_CART",
+            payload: {
+                products: deletedCartList
+            }
+        })
+    }
+
+    const updateTotal = (products) =>{
+        let total = 0;
+        products.forEach(product => total = total + product.price);
+
+        dispatch({
+            type: "UPDATE_TOTAL",
+            payload: {
+                total
+            }
+        })
+    }
+
+    const value = {
+        total: state.total,
+        cartList: state.cartList,
+        addToCart,
+        removeFromCart
+    };
+    return (
+        <CartContext.Provider value={value}>
+            {children}
+        </CartContext.Provider>  
+    );
+}
+
+export const useCart = () => {
+    return useContext(CartContext);
+}
